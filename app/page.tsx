94b54c601bdac425ad418ec395d38f47b778aa7e
29-Totoro <duykhoi2k7@gmail.com>
2025-10-31 20:43:51 +0700

    save

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className="text-4xl font-bold text-black dark:text-white">
-        Hello World 🌍 khoi
+        Hello World 🌍
       </h1>
     </div>
   );
